f12a20f88164bbd2fdb4b6942fb41e2c1baee048
Sheetal <sheetalamenhadatt@gmail.com>
2018-08-23 12:21:22 +1200

    First Version

---
+++ src/main/java/com/aut/em11683/TravisSpringDemo/SimpleMath.java
@@ -0,0 +1,74 @@
+package com.aut.em11683.TravisSpringDemo;
+
+
+public class SimpleMath {
+
+	/**
+	 * Stores the number of performed calculations.
+	 */
+	private long operations;
+
+	/**
+	 * Standard Constructor
+	 */
+	public SimpleMath() {
+		operations = 0;
+	}
+
+	/**
+	 * Adds two numbers.
+	 * 
+	 * @param a
+	 * @param b
+	 * @return a + b
+	 */
+	public double add(double a, double b) {
+		operations++;
+		return a + b;
+	}
+
+	/**
+	 * Subtracts two numbers from each other.
+	 * 
+	 * @param a
+	 * @param b
+	 * @return a - b
+	 */
+	public double subtract(double a, double b) {
+		operations++;
+		return a - b;
+	}
+
+	/**
+	 * Divides two numbers.
+	 * 
+	 * @param a
+	 * @param b
+	 * @return a / b
+	 */
+	public double divide(double a, double b) {
+		operations++;
+		return a / b;
+	}
+
+	/**
+	 * Multiply two numbers.
+	 * 
+	 * @param a
+	 * @param b
+	 * @return a * b
+	 */
+	public double multiply(double a, double b) {
+		operations++;
+		return a * b;
+	}
+
+	/**
+	 * Returns the number of calculations performed.
+	 * 
+	 * @return number of calculations performed
+	 */
+	public long getNumberOfCalculatedOperations() {
+		return operations;
+	}
+}
---
+++ src/main/java/com/aut/em11683/TravisSpringDemo/TravisSpringDemoApplication.java
@@ -0,0 +1,12 @@
+package com.aut.em11683.TravisSpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisSpringDemoApplication.class, args);
+	}
+}
